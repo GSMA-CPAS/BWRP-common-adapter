db19b745ffc34c27076eb1167ed8950d05e29395
Christophe Sarthou-Camy <christophe@mbpdechristophe.home>
2020-11-16 18:29:11 +0100

    [WIP] Create BlockchainAdapterProvider

--- server/config.js
+++ server/config.js
@@ -12,4 +12,11 @@ config.OPENAPI_YAML = path.join(config.ROOT_DIR, 'api', 'openapi.yaml');
 config.FULL_PATH = `${config.URL_PATH}:${config.URL_PORT}/${config.BASE_VERSION}`;
 config.FILE_UPLOAD_PATH = path.join(config.PROJECT_DIR, 'uploaded_files');
 
+// Logger configuration
+config.LOG_LEVEL = process.env.LOG_LEVEL || "info";
+
+// BlockchainAdapter configuration
+config.BLOCKCHAIN_ADAPTER_URL = process.env.BLOCKCHAIN_ADAPTER_URL || "http://127.0.0.1:8081";
+config.BLOCKCHAIN_ADAPTER_WEBHOOKS = process.env.BLOCKCHAIN_ADAPTER_WEBHOOKS || [];
+
 module.exports = config;
