b5127adb79feb95a707eb20a4c3ee31d8190f584
zkong <zkong@gsma.com>
2020-11-23 14:56:26 +0000

    - added mongo-init.js

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "user",
+        pwd: "userpw",
+        roles: [
+            {
+                role: "readWrite",
+                db: "commondb"
+            }
+        ]
+    }
+);
