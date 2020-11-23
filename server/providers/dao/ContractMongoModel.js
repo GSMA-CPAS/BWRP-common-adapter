383a1aec15e56749a458a0c73d5988088d80b679
Christophe Sarthou-Camy <christophe.sarthoucamy@orange.com>
2020-11-23 15:29:58 +0100

    [FEATURE] Create contract model in DB

--- server/providers/dao/ContractMongoModel.js
+++ server/providers/dao/ContractMongoModel.js
@@ -3,10 +3,50 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+var HistorySchema = new Schema({ 
+  date: { type: Date, required: true },
+  action: { type: String, required: true }
+}, { _id: false });
+
+var SignatureSchema = new Schema({ 
+  minSignatures: { type: Number, required: true, min: 1 }
+}, { _id: false });
+
+var BankDetails = Schema.Types.Mixed;
+
+var DiscountModelsSchema = Schema.Types.Mixed;
+
+var GeneralInformationSchema = new Schema({ 
+  name: { type: String, required: false },
+  type: { type: String, required: false },
+  startDate: { type: Date, required: false },
+  endDate: { type: Date, required: false },
+  prolongationLength: { type: String, required: false }
+}, { _id: false });
+
+var ContractBodySchema = new Schema({ 
+  taps: { type: [String], required: false },
+  bankDetails: { type: Map, of: BankDetails, required: true },
+  discountModels: { type: DiscountModelsSchema, required: false },
+  generalInformation: { type: GeneralInformationSchema, required: false }
+}, { _id: false });
+
+var ContractHeaderSchema = new Schema({ 
+  msps: { type: Map, of: SignatureSchema, required: true }
+}, { _id: false });
+
 var ContractSchema = new Schema({ 
-  id: { type:String, required: true},
-  name: { type:String, required: true},
-  state: { type:[String], required: true}
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  fromMspId: { type: String, required: true },
+  toMspId: { type: String, required: true },
+  header: { type: ContractHeaderSchema, required: true },
+  body: { type: ContractBodySchema, required: true },
+  rawData: { type: String, required: true },
+  state: { type: String, required: true },
+  history: { type: [HistorySchema], required: true },
+  creationDate: { type: Date, required: true },
+  lastModificationDate: { type: Date, required: true, default: Date.now }
 });
 
 ContractSchema.index({ id: 1 }, { unique: true });
---
+++ server/test/tools/testsDbUtils.js
@@ -0,0 +1,80 @@
+const debug = require('debug')('spec:testsDbUtils');
+
+const ContractMongoRequester = require('../../providers/dao/ContractMongoRequester');
+
+class TestsDbUtils {
+
+  static removeAllContracts(conditions) {
+    return new Promise((resolve, reject) => {
+      ContractMongoRequester.deleteMany(conditions, (err, contracts) => {
+        if (err) {
+          debug("deleteMany contracts failure : ", err);
+          reject(err);
+        } else {
+          debug("deleteMany contracts done with success");
+          resolve(contracts);  
+        }
+      });
+    });
+  }
+
+  static createContract(contract) {
+    return new Promise((resolve, reject) => {
+
+      // Define automatic values
+      contract.id = ContractMongoRequester.defineContractId();
+      if (contract.creationDate === undefined) {
+        contract.creationDate = Date.now();
+      }
+      if (contract.state === undefined) {
+        contract.state = "DRAFT";
+      }
+      if (contract.history === undefined) {
+        contract.history = [];
+      }
+      contract.history.push({
+        date: contract.creationDate,
+        action: "CREATION"
+      });
+
+      // Launch db request
+      ContractMongoRequester.create(contract, (err, createdContract) => {
+        if (err) {
+          debug("create contract failure : ", err);
+          reject(err);
+        } else {
+          debug("create contract done with success");
+          resolve(createdContract);  
+        }
+      });
+    });
+  }
+
+  static initDbWithContracts(contracts) {
+    return new Promise((resolve, reject) => {
+      TestsDbUtils.removeAllContracts({})
+        .then(removeAllContractsResp => {
+          const contractsCreationPromises = [];
+          if ((contracts !== undefined) && (Array.isArray(contracts))) {
+            contracts.forEach(contract => {
+              contractsCreationPromises.push(TestsDbUtils.createContract(contract));
+            })
+          }
+          Promise.all(contractsCreationPromises)
+            .then(contractsCreationPromisesResp => {
+              debug("initDbWithContracts done with success");
+              resolve(contractsCreationPromisesResp);
+            })
+            .catch(contractsCreationPromisesError => {
+              debug("initDbWithContracts failure : ", contractsCreationPromisesError);
+              reject(contractsCreationPromisesError);
+            });
+        })
+        .catch(removeAllContractsError => {
+          reject(removeAllContractsError);
+        });
+    });
+  }  
+
+}
+module.exports = TestsDbUtils;
