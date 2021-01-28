721f3d0a788972dac294701a568095986d717173
Christophe Sarthou-Camy <christophe.sarthoucamy@orange.com>
2021-01-28 17:34:20 +0100

    [FEATURE] Return an error http 422 when the contract to update is not DRAFT

--- server/test/api/0003_PUT_contract.spec.js
+++ server/test/api/0003_PUT_contract.spec.js
@@ -45,13 +45,30 @@ describe(`Tests PUT ${route} API OK`, function() {
       rawData: 'Ctr_raw-data-2'
     };
 
+    const contract3 = {
+      name: 'Contract name between A1 and D4',
+      state: 'SENT',
+      type: 'contract',
+      version: '1.4.1',
+      fromMsp: {mspId: 'A1'},
+      toMsp: {mspId: 'D4'},
+      referenceId: 'AZRAGGSHJIAJAOJSNJNSSNNAIT',
+      body: {
+        bankDetails: {A1: {iban: null, bankName: null, currency: null}, C3: {iban: null, bankName: null, currency: null}},
+        discountModels: 'someData',
+        generalInformation: {name: 'test3', type: 'Normal', endDate: '2021-01-01T00:00:00.000Z', startDate: '2020-12-01T00:00:00.000Z'}
+      },
+      rawData: 'Ctr_raw-data-3'
+    };
+
     before((done) => {
-      debugSetup('==> init db with 2 contracts');
-      testsDbUtils.initDbWithContracts([contract1, contract2])
+      debugSetup('==> init db with 3 contracts');
+      testsDbUtils.initDbWithContracts([contract1, contract2, contract3])
         .then((initDbWithContractsResp) => {
           contract1.id = initDbWithContractsResp[0].id;
           contract2.id = initDbWithContractsResp[1].id;
-          debugSetup('The db is initialized with 2 contracts : ', initDbWithContractsResp.map((c) => c.id));
+          contract3.id = initDbWithContractsResp[2].id;
+          debugSetup('The db is initialized with 3 contracts : ', initDbWithContractsResp.map((c) => c.id));
           debugSetup('==> done!');
           done();
         })
@@ -209,6 +226,51 @@ describe(`Tests PUT ${route} API OK`, function() {
             // expect(Object.keys(response.body.history[1])).have.members(["date", "action"]);
             // expect(response.body.history[1]).to.have.property('date').that.is.a('string').and.match(DATE_REGEX);
             // expect(response.body.history[1]).to.have.property('action', 'UPDATE');
+
+            done();
+          });
+      } catch (exception) {
+        debug('exception: %s', exception.stack);
+        expect.fail('it test throws an exception');
+        done();
+      }
+    });
+
+    it('Put contract REJECTED for SENT contract', function(done) {
+      try {
+        const path = globalVersion + '/contracts/' + contract3.id;
+        debug('path : ', path);
+
+        const sentBody = {
+          header: {
+            name: 'Contract name between A2 and D4 - v2',
+            state: 'DRAFT',
+            version: '5.7',
+            type: 'contract',
+            fromMsp: {mspId: 'A2', signatures: [{id: 'signatureId_1_InString', name: 'employeeName_A2_1', role: 'financeDirector'}]},
+            toMsp: {mspId: 'D4', signatures: [{id: 'signatureId_C1_1_InString', name: 'employeeName_C1_1', role: 'financeDirector'}, {id: 'signatureId_C1_2_InString', name: 'employeeName_C1_2', role: 'saleDirector'}]}
+          },
+          body: {
+            randomBodyContent: 'This random field is not defined in schema',
+            bankDetails: {A2: {iban: '##IBAN-A2-1##', bankName: '##BANK-A2-1##', currency: 'Dollars'}, C1: {iban: '##IBAN-C1-3##', bankName: '##BANK-C1-3##', currency: 'Euros'}},
+            discountModels: 'someDiscountModelToDefine',
+            generalInformation: {name: 'test4-2', type: 'Normal', endDate: '2021-01-01T00:00:00.000Z', startDate: '2020-12-01T00:00:00.000Z'}
+          }
+        };
+
+        chai.request(testsUtils.getServer())
+          .put(`${path}`)
+          .send(sentBody)
+          .end((error, response) => {
+            expect(error).to.be.null;
+            expect(response).to.have.status(422);
+            expect(response).to.be.json;
+            expect(response.body).to.exist;
+            expect(response.body).to.be.an('object');
+
+            expect(response.body).to.have.property('internalErrorCode', 2000);
+            expect(response.body).to.have.property('message', 'Contract modification not allowed');
+            expect(response.body).to.have.property('description', `It's not allowed to update this contract or its state.`);
 
             done();
           });
