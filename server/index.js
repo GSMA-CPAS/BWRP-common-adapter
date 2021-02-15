a74e278af7a1da55106ff4351baa8c1422a1b400
Christophe Sarthou-Camy <christophe.sarthoucamy@orange.com>
2021-02-15 12:42:25 +0100

    [Fix] Change variables definitions on server start

--- server/index.js
+++ server/index.js
@@ -2,18 +2,20 @@ const logger = require('./logger');
 const ExpressServer = require('./expressServer');
 const serverPort = 3000;
 
+let expressServer = undefined;
+
 const launchServer = async () => {
-    try {
-        this.expressServer = new ExpressServer(serverPort, 'api/openapi.yaml');
-        this.expressServer.launch();
-        logger.info('Express server running');
-    } catch (error) {
-        logger.error('Express Server failure', error.message);
-        await this.close();
-    }
+  try {
+    expressServer = new ExpressServer(serverPort, 'api/openapi.yaml');
+    expressServer.launch();
+    logger.info('Express server running');
+  } catch (error) {
+    logger.error('Express Server failure', error.message);
+    await this.close();
+  }
 };
 
 launchServer().catch((e) => logger.error(e));
 
 // for tests
-module.exports = this.expressServer.app;
+module.exports = expressServer.app;
