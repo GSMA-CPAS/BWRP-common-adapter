ac025d0702b5ae8329c693448374826d8acd9b23
Guillaume Jourdain <guillaume.jourdain@orange.com>
2021-07-19 16:26:02 +0200

    [FEATURE] tests for getUsageById with RAW format

--- server/test/api/0202_GET_usage.spec.js
+++ server/test/api/0202_GET_usage.spec.js
@@ -76,6 +76,23 @@ describe(`Tests GET ${route} API OK`, function() {
       },
       state: 'DRAFT'
     };
+    const usage2 = {
+      type: 'usage',
+      version: '1.1.0',
+      name: 'Usage data SENT',
+      contractId: contract1.id,
+      mspOwner: 'A1',
+      mspReceiver: 'B1',
+      referenceId: 'AZRAGGSHJIAJAOJSNJNSSNNAIS',
+      blockchainRef: {type: 'hlf', txId: 'TX-RAGGSHJIAJAOJSNJNSSNNAIS', timestamp: new Date().toJSON()},
+      body: {
+        data: [
+          {year: 2020, month: 1, hpmn: 'HPMN', vpmn: 'VPMN', service: 'service', value: 1, units: 'unit', charges: 'charge', taxes: 'taxes'}
+        ]
+      },
+      state: 'SENT',
+      rawData: 'Ctr_raw-data-2'
+    };
 
     before((done) => {
       debugSetup('==> init db with 2 contracts');
@@ -86,10 +103,13 @@ describe(`Tests GET ${route} API OK`, function() {
           contract2.id = initDbWithContractsResp[1].id;
           usage1.contractId = contract1.id;
           usage1.mspOwner = contract1.fromMsp.mspId;
-          testsDbUtils.initDbWithUsages([usage1])
+          usage2.contractId = contract1.id;
+          usage2.mspOwner = contract1.fromMsp.mspId;
+          testsDbUtils.initDbWithUsages([usage1, usage2])
             .then((initDbWithUsagesResp) => {
-              debugSetup('One usage document linked to contract ', initDbWithUsagesResp[0].contractId);
+              debugSetup('Two usage documents linked to contract ', initDbWithUsagesResp[0].contractId);
               usage1.id = initDbWithUsagesResp[0].id;
+              usage2.id = initDbWithUsagesResp[1].id;
               debugSetup('==> done!');
               done();
             })
@@ -106,7 +126,7 @@ describe(`Tests GET ${route} API OK`, function() {
         });
     });
 
-    it('GET usage OK', function(done) {
+    it('GET usage OK in JSON format', function(done) {
       try {
         const path = globalVersion + '/contracts/' + contract1.id + '/usages/' + usage1.id;
         debug('GET path : ', path);
@@ -139,6 +159,73 @@ describe(`Tests GET ${route} API OK`, function() {
             expect(response.body).to.have.property('body').that.is.an('object');
             expect(Object.keys(response.body.body)).have.members(['data']);
             expect(response.body.body).to.deep.include(usage1.body);
+
+            done();
+          });
+      } catch (exception) {
+        debug('exception: %s', exception.stack);
+        expect.fail('it test throws an exception');
+        done();
+      }
+    });
+
+    it('GET usage OK in RAW format', function(done) {
+      try {
+        const path = globalVersion + '/contracts/' + contract1.id + '/usages/' + usage2.id + '?format=RAW';
+        debug('GET path : ', path);
+
+        chai.request(testsUtils.getServer())
+          .get(`${path}`)
+          .end((error, response) => {
+            debug('response.status: %s', JSON.stringify(response.status));
+            debug('response.body: %s', JSON.stringify(response.body));
+            expect(error).to.be.null;
+            expect(response).to.have.status(200);
+            expect(response).to.be.json;
+            expect(response.body).to.exist;
+            expect(response.body).to.be.an('object');
+            expect(Object.keys(response.body)).have.members(['usageId', 'state', 'referenceId', 'blockchainRef', 'creationDate', 'lastModificationDate', 'raw']);
+
+            expect(response.body).to.have.property('usageId', usage2.id);
+            expect(response.body).to.have.property('state', usage2.state);
+            expect(response.body).to.have.property('referenceId', usage2.referenceId);
+            expect(response.body).to.have.property('raw', usage2.rawData);
+            expect(response.body).to.have.property('creationDate').that.is.a('string').and.match(DATE_REGEX);
+            expect(response.body).to.have.property('lastModificationDate').that.is.a('string').and.match(DATE_REGEX);
+            expect(response.body).to.have.property('blockchainRef').that.is.an('object');
+            expect(Object.keys(response.body.blockchainRef)).have.members(['type', 'txId', 'timestamp']);
+            expect(response.body.blockchainRef).to.have.property('type', 'hlf');
+            expect(response.body.blockchainRef).to.have.property('txId', usage2.blockchainRef.txId);
+            expect(response.body.blockchainRef).to.have.property('timestamp').that.is.a('string');
+
+            done();
+          });
+      } catch (exception) {
+        debug('exception: %s', exception.stack);
+        expect.fail('it test throws an exception');
+        done();
+      }
+    });
+
+    it('GET usage FAILED in RAW format for DRAFT usage', function(done) {
+      try {
+        const path = globalVersion + '/contracts/' + contract1.id + '/usages/' + usage1.id + '?format=RAW';
+        debug('GET path : ', path);
+
+        chai.request(testsUtils.getServer())
+          .get(`${path}`)
+          .end((error, response) => {
+            debug('response.status: %s', JSON.stringify(response.status));
+            debug('response.body: %s', JSON.stringify(response.body));
+
+            expect(error).to.be.null;
+            expect(response).to.have.status(422);
+            expect(response.body).to.exist;
+            expect(response.body).to.be.an('object');
+
+            expect(response.body).to.have.property('internalErrorCode', 2028);
+            expect(response.body).to.have.property('message', 'Unvailable RAW format');
+            expect(response.body).to.have.property('description', 'The RAW format of this usage is unavailable.');
 
             done();
           });
