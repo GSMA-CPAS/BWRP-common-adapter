e9576e439c767598d1c59a73b55a38cbc88a3b2d
zkong <zkong@gsma.com>
2021-09-21 11:53:21 +0100

    isUsageApproved for Contracts

--- server/services/EventService.js
+++ server/services/EventService.js
@@ -283,6 +283,29 @@ const eventSignatureReceived = ({body}) => new Promise(
           const unsignedNumber = updateUsageResp.signatureLink.filter((signature) => (signature['txId'] === undefined)).length;
           if (unsignedNumber == 0) {
             const updateUsageWithTagResp = await LocalStorageProvider.updateUsageWithTag(updateUsageResp.id, 'APPROVED');
+
+            let signatureSent = 0;
+            let signatureReceived = 0;
+            const sentUsage = await LocalStorageProvider.getUsages(usageToUpdate.contractId, {state: 'SENT'});
+            if ((sentUsage !== undefined) && (Array.isArray(sentUsage))) {
+              sentUsage.forEach((usage) => {
+                if (usage.tag=='APPROVED') {
+                  signatureSent++;
+                }
+              });
+            }
+
+            const receivedUsage = await LocalStorageProvider.getUsages(usageToUpdate.contractId, {state: 'RECEIVED'});
+            if ((receivedUsage !== undefined) && (Array.isArray(receivedUsage))) {
+              receivedUsage.forEach((usage) => {
+                if (usage.tag=='APPROVED') {
+                  signatureReceived++;
+                }
+              });
+            }
+            if (signatureSent >= 1 && signatureReceived >= 1) {
+              await LocalStorageProvider.updateContractIsUsageApproved(usageToUpdate.contractId, true);
+            }
           }
         }
       }
